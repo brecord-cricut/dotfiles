47d4e143cc18873190eb1069a092d3d72ef8e69f
Ben Record <brecord@cricut.com>
2025-06-06 17:18:24 -0600

    Neovim - Disable auto pairs when recording macros

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,21 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("user_" .. name, { clear = true })
+end
+
+-- Disable autopairs when recording a macro
+local minipairs_state
+vim.api.nvim_create_autocmd({ "RecordingEnter", "RecordingLeave" }, {
+  group = augroup("macro_no_pairs"),
+  callback = function(ev)
+    if ev.event == "RecordingEnter" then
+      minipairs_state = vim.g.minipairs_disable or false
+      vim.g.minipairs_disable = true
+    else
+      vim.g.minipairs_disable = minipairs_state
+    end
+  end,
+})
