c93ef1078412cee6b521c6422d0433fdc9641eee
Ben Record <brecord@cricut.com>
2025-06-06 15:06:07 -0600

    Neovim - Git rebase opts

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -11,16 +11,45 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("user_" .. name, { clear = true })
 end
 
--- Disable autopairs when recording a macro
-local minipairs_state
-vim.api.nvim_create_autocmd({ "RecordingEnter", "RecordingLeave" }, {
-  group = augroup("macro_no_pairs"),
-  callback = function(ev)
-    if ev.event == "RecordingEnter" then
-      minipairs_state = vim.g.minipairs_disable or false
-      vim.g.minipairs_disable = true
-    else
-      vim.g.minipairs_disable = minipairs_state
-    end
+-- quit some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("quit_with_q"),
+  pattern = {
+    "gitcommit",
+    "gitrebase",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd("quit")
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit Vim",
+      })
+    end)
+  end,
+})
+
+-- close some filetypes with <q>, extends LazyVim's existing buffer list
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "dap-float",
+    "log",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd("close")
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
   end,
 })
