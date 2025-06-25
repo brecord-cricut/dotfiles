fcad0ffe609b56da62a8287e17ac5c195e585bfe
Ben Record <brecord@cricut.com>
2025-06-25 09:18:21 -0600

    Updates to Cricut aliases

--- .config/zsh/custom/cricut.zsh
+++ .config/zsh/custom/cricut.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh
 
+alias hd="heimdall"
 alias dhd="cd ~/src/heimdall"
 alias dhdr="cd $XDG_DATA_HOME/repos/heimdall"
-alias notes="cd ~/Nextcloud/Notes && $EDITOR -c \"lua require('persistence').load()\" && cd -"
