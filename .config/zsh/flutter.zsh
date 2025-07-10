b7ca37d21025b59771b691fa4549bdcd197a1abd
Ben Record <brecord@cricut.com>
2025-07-10 09:29:09 -0600

    Adding flutter global bin to `PATH`

--- .config/zsh/flutter.zsh
+++ .config/zsh/flutter.zsh
@@ -4,7 +4,7 @@ flutter_home="$XDG_DATA_HOME/repos/flutter"
 
 if [[ -d "$flutter_home" ]]; then
   export FLUTTER_HOME="$XDG_DATA_HOME/repos/flutter"
-  export PATH="$FLUTTER_HOME/bin:$PATH"
+  export PATH="$FLUTTER_HOME/bin:$PATH:$HOME/.pub-cache/bin"
 
   [[ $(uname) == "Darwin" ]] && alias flutter="flutter -d macos"
 fi
